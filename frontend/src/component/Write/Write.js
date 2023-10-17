acde9a37742227ea6a2e93a11f62681de59368d4
ejeonghun <wjdgns4019@gmail.com>
2023-10-17 10:19:18 +0900

    충돌 해결

--- frontend/src/component/Write/Write.js
+++ frontend/src/component/Write/Write.js
@@ -1,4 +1,4 @@
-import React, { useContext} from 'react';
+import React, { useContext, useState} from 'react';
 import styled from 'styled-components'
 import AuthContext from '../AuthContext/AuthContext';
 
