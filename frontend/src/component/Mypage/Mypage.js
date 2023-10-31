d6bbb8abb4f9f6e7c29845a865ee4d299b72aa7b
이우석 <useok@iuseog-ui-MacBookAir.local>
2023-10-31 12:45:54 +0900

    마이페이지 스타일

--- frontend/src/component/Mypage/Mypage.js
+++ frontend/src/component/Mypage/Mypage.js
@@ -14,51 +14,50 @@ function Mypage() {
     return (
         <div className="Mypage">
             <div className="Mypage_title">
+                <div className="Mypage_title_text">
+                    <h2 className="nickname">{nickname}님</h2>
+                    <div className="Mypage_title_point">
+                <Link to="/PointHistory" className="point_history">
+                <h4>100,000P ▶</h4>
+                </Link>
+                    </div>
+                </div>
                 <div className="Mypage_title_img">
-                    <img src={profileImage} alt="프로필 사진" className="profile_img"></img> {/* 프로필 이미지 추가 */} 
-                </div>
-                <h2 className="nickname">{nickname}님</h2>
-                <div className="Mypage_title_point">
-                    <h3 className="margin0">내 포인트</h3>
-                    <Link to="/PointHistory" className="point_history">
-                        <h4>100,000P ▶</h4>
-                    </Link>
+                    <img src={profileImage} alt="프로필 사진" className="profile_img"></img>
                 </div>
             </div>
-            <div className="Mypage_info">
-                <h4 className="mypage_text">마이페이지</h4>
-                <div className="icon_text">
-                    <Link to="charge" className="icon_link">
-                        <img src="./img/money.png" alt="충전" className="icon_img"></img>
-                        <h2>충전하기</h2>
-                    </Link>
-                </div>
-                <div className="icon_text">
-                    <Link to="/giftshop" className="icon_link">
-                        <img src="./img/shopping.png" alt="" className="icon_img"></img>
-                        <h2>포인트 샵</h2>
-                    </Link>
-                </div>
-                <div className="icon_text">
-                    <Link to="wrote" className="icon_link">
-                        <img src="./img/pencil.png" alt="" className="icon_img"></img>
-                        <h2>작성한 글</h2>
-                    </Link>
-                </div>
-                <div className="icon_text">
-                    <Link to="comment" className="icon_link">
-                        <img src="./img/comment.png" alt="" className="icon_img"></img>
-                        <h2>작성한 댓글</h2>
-                    </Link>
-                </div>
-                <div className="icon_text_border">
-                    <Link to="myvote" className="icon_link">
-                        <img src="./img/check-box.png" alt="" className="icon_img"></img>
-                        <h2>참여한 투표</h2>
-                    </Link>
-                </div>
-            </div>
+    <div className="Mypage_info">
+    <h4 className="mypage_text">마이페이지</h4>
+    <div className="icon_group_center">
+        <div className="icon_text">
+            <Link to="charge" className="icon_link">
+                <img src="./img/money.png" alt="충전" className="icon_img"></img>
+                <h2>충전하기</h2>
+            </Link>
+            <Link to="/giftshop" className="icon_link">
+                <img src="./img/shopping.png" alt="" className="icon_img"></img>
+                <h2>포인트 샵</h2>
+            </Link>
         </div>
+    </div>
+    <div className="icon_group_bottom">
+        <div className="icon_text">
+            <Link to="wrote" className="icon_link">
+                <img src="./img/pencil.png" alt="" className="icon_img"></img>
+                <h2>작성한 글</h2>
+            </Link>
+            <Link to="comment" className="icon_link">
+                <img src="./img/comment.png" alt="" className="icon_img"></img>
+                <h2>작성한 댓글</h2>
+            </Link>
+            <Link to="myvote" className="icon_link">
+                <img src="./img/check-box.png" alt="" className="icon_img"></img>
+                <h2>참여한 투표</h2>
+            </Link>
+        </div>
+    </div>
+</div>
+</div>
     );
 };}
 
